139edc2c03113cc72e81fa997f158e636612664e
lukmanahromi <lukmanahromi@gmail.com>
2025-03-01 14:22:33 +0700

    [ADD] - helpers getTestObject, basic login test

--- Keywords/com/ahromi/LoginPage/LoginScreen.groovy
+++ Keywords/com/ahromi/LoginPage/LoginScreen.groovy
@@ -10,11 +10,11 @@ import com.ahromi.BaseHelper
 import com.kms.katalon.core.annotation.Keyword
 import com.kms.katalon.core.checkpoint.Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling
 import com.kms.katalon.core.testcase.TestCase
 import com.kms.katalon.core.testdata.TestData
 import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.util.KeywordUtil
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
@@ -22,26 +22,40 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable
 
 public class LoginScreen extends BaseHelper {
-	
+
 	private TestObject lblTitleLogin
-	private TestObject lblDescLogin
 	private TestObject btnLogin
 	private TestObject txfUsername
 	private TestObject txfPassword
-	
+	private TestObject lblInvalid
+
 	public LoginScreen() {
-		lblTitleLogin	= getObjectByXpath("lblTitleLogin", "//android.widget.TextView[@resource-id = 'login-title']")
-		lblDescLogin	= getObjectByXpath("", "")
-		btnLogin		= getObjectByXpath("", "")
-		txfUsername		= getObjectByXpath("", "")
-		txfPassword		= getObjectByXpath("", "")
+		lblTitleLogin	= getTestObject("lblTitleLogin", "login_logo", "class")
+		txfUsername		= getTestObject("txfUsername", "user-name", "id")
+		txfPassword		= getTestObject("txfPassword", "password", "id")
+		btnLogin		= getTestObject("btnLogin", "login-button", "id")
+		lblInvalid		= getTestObject("lblInvalid", "error-button", "class")
 	}
-	
+
 	public void clickLogin() {
-		
+		WebUI.click(btnLogin)
 	}
-	
+
 	public void verifyLandingOnLoginScreen() {
 		verifyLanding(lblTitleLogin, "Login Screen")
+	}
+	
+	public void inputUsername(String username) {
+		WebUI.setText(txfUsername, username)
+	}
+	
+	public void inputPassword(String password) {
+		WebUI.setText(txfPassword, password)
+	}
+	
+	public void verifyInvalidInput() {
+		if (WebUI.verifyElementVisible(lblInvalid)) {
+			WebUI.getText(lblInvalid)
+		}
 	}
 }
